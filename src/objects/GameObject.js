e7e14447c1028de23493183ed57d25a57bdd6332
adamsko <adam@skobo.pl>
2016-01-17 19:24:07 +0100

    GameObject update. proper display.

--- src/objects/GameObject.js
+++ src/objects/GameObject.js
@@ -1,48 +1,50 @@
 function GameObject(game, group, tileObject) {
-	this.game = game;
 	this.group = group;
-	this.tileObject = tileObject;
-	this.tileObject.angle = tileObject.properties.rot;
-	if (this.tileObject.angle === undefined)
-		this.tileObject.angle = 0;
-	else
-		this.tileObject.angle = parseInt(this.tileObject.angle);
-
-	this.gamePos = getGamePos(this.tileObject.x, this.tileObject.y);
+	this.gamePos = posTiledToGame(tileObject.x, tileObject.y);
+	this.sprite = {};
 
 	var objectData = gameObjectsContants[tileObject.name];
-
-	var screenPos = this.getScreenPos();
-	this.sprite = this.group.create(screenPos[0], screenPos[1],
+	this.sprite = this.group.create(0, 0,
 		objectData.spreadsheet, tileObject.gid - 1);
-
-	this.sprite.scale.x = scaleConstants.MAIN_SCALE;
-	this.sprite.scale.y = scaleConstants.MAIN_SCALE;
-	this.rotate(this.tileObject.angle);
-};
-
-GameObject.prototype.rotate = function(angle) {
-	console.log(this.sprite.anchor);
-	this.sprite.anchor.x = 0.5;
-	this.sprite.angle = angle;
+	if (tileObject.properties.rot === undefined)
+		this.sprite.angle = 0;
+	else
+		this.sprite.angle = parseInt(tileObject.properties.rot);
+	this.sprite.anchor.x = this.sprite.anchor.y = 0.5;
+	this.sprite.scale.x = this.sprite.scale.y = scaleConstants.MAIN_SCALE;
+	this.updateScreenPos();
 }
 
 /**
- * Get game position (0,0; 1,1 etc)from Tiled position.
- * @param  {[type]} tileObject [description]
- * @return {[type]}            [description]
+ * Update sprite's screen pos. After this. object should be properly 
+ * placed on the screen.
  */
-function getGamePos(tiledPosX, tiledPosY) {
-	var gamePos = [
-		tiledPosX / scaleConstants.TILE_SIZE,
-		(tiledPosY - scaleConstants.TILE_SIZE) / scaleConstants.TILE_SIZE
-	];
-	return gamePos;
+GameObject.prototype.updateScreenPos = function() {
+	var screenPos = this.getScreenPos();
+	this.sprite.x = screenPos[0];
+	this.sprite.y = screenPos[1];
+	this.sprite.x += scaleConstants.TILE_SIZE_SCALED / 2;
+	this.sprite.y += scaleConstants.TILE_SIZE_SCALED / 2;
 }
 
+/**
+ * Convert game position to screen position.
+ * @return {[type]} [description]
+ */
 GameObject.prototype.getScreenPos = function() {
 	return [
 		this.gamePos[0] * scaleConstants.TILE_SIZE_SCALED,
 		this.gamePos[1] * scaleConstants.TILE_SIZE_SCALED
 	];
+}
+
+/**
+ * TODO: not a method. Move to somewhere more general.
+ * Get game position (0,0; 1,1 etc)from Tiled position.
+ */
+function posTiledToGame(tiledPosX, tiledPosY) {
+	var gamePos = [
+		tiledPosX / scaleConstants.TILE_SIZE, (tiledPosY - scaleConstants.TILE_SIZE) / scaleConstants.TILE_SIZE
+	];
+	return gamePos;
 }
